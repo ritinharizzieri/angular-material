6537d1893ed8970f42dd75833d1e2f8d93063119
Rita Rizzieri <rita.rizzieri@checklistfacil.com>
2023-03-26 12:46:46 -0300

    adicionando rxjs no sidenav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { MatSidenav } from '@angular/material/sidenav';
+import {BreakpointObserver} from '@angular/cdk/layout'
+import { Component, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-material';
+  @ViewChild(MatSidenav) sidenav!: MatSidenav
+  constructor(private breakpointObserver: BreakpointObserver) { }
+
+  ngAfterContentInit(): void {
+    this.breakpointObserver.observe(['(max-width: 800px)']).subscribe({
+      next:(res) =>{
+        if(res.matches){
+          this.sidenav.mode = 'over';
+          this.sidenav.close();
+        } else {
+          this.sidenav.mode = 'side';
+          this.sidenav.open();
+        }
+      }
+    })
+  }
+
+
 }
