27bb11f70cd696cf22dc6a478f3fdb7e75fb3fb7
Rita Rizzieri <rita.rizzieri@checklistfacil.com>
2023-04-10 08:11:19 -0300

    monta a toolbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { MatSidenav } from '@angular/material/sidenav';
 import {BreakpointObserver} from '@angular/cdk/layout'
-import { Component, ViewChild } from '@angular/core';
+import { Component,  } from '@angular/core';
+import { fromEvent } from "rxjs";
+import { map } from "rxjs/operators";
 
 @Component({
   selector: 'app-root',
@@ -17,6 +19,13 @@ export class AppComponent {
     this.breakpointObserver.observe(['(max-width: 800px)'])
     // quando tem apenas uma função no next e ela já vai fazer tudo que precisa fazer, dai não precisa usar o next.
     .subscribe((res) => this.isSmallScreen = res.matches)
+
+    const height = fromEvent(window, "wheel").pipe(
+      map((e) => e.currentTarget?.dispatchEvent)
+    );
+
+    console.log('height', height.subscribe((e) => console.log(e)));
+
   }
 
   get sidenavMode(){
