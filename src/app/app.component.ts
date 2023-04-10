823790829302c3fa99badd9d4a02a0d0f91fe945
Rita Rizzieri <rita.rizzieri@checklistfacil.com>
2023-04-10 08:34:24 -0300

    conclusão do desafio da toolbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,42 @@
 import { MatSidenav } from '@angular/material/sidenav';
 import {BreakpointObserver} from '@angular/cdk/layout'
-import { Component,  } from '@angular/core';
+import { Component, OnInit,  } from '@angular/core';
 import { fromEvent } from "rxjs";
 import { map } from "rxjs/operators";
+
+export const SCROLL_CONTAINER = 'mat-sidenav-content'
+export const TEXT_LIMIT = 50
+export const SHADOW_LIMIT = 100;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   public isSmallScreen:boolean = false;
+  public popText = false
+  public applyShadow = false
 
   constructor(private breakpointObserver: BreakpointObserver) { }
+
+  ngOnInit(){
+    const content = document.getElementsByClassName(SCROLL_CONTAINER)[0];
+
+    fromEvent(content, 'scroll')
+    .pipe(
+      map(() => content.scrollTop)
+    )
+    .subscribe({
+      next:(value) => this.determineHeader(value)
+    })
+
+  }
+
+  determineHeader(top: number){
+    this.popText = top >= TEXT_LIMIT
+    this.applyShadow = top >= SHADOW_LIMIT
+  }
 
   ngAfterContentInit(): void {
     // Esse observe esta analisando o tempo todo o tamanho da tela
@@ -23,8 +47,6 @@ export class AppComponent {
     const height = fromEvent(window, "wheel").pipe(
       map((e) => e.currentTarget?.dispatchEvent)
     );
-
-    console.log('height', height.subscribe((e) => console.log(e)));
 
   }
 
