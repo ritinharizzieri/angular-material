571e6808033af0cad6dde5a6431102722e0aacb7
Rita Rizzieri <rita.rizzieri@checklistfacil.com>
2023-04-12 08:45:21 -0300

    criando funcao de rxjs com o progress do spinner

--- src/app/progress-spinner/progress-spinner.component.ts
+++ src/app/progress-spinner/progress-spinner.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {interval, map, takeWhile} from 'rxjs'
 
 @Component({
   selector: 'app-progress-spinner',
@@ -7,9 +8,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProgressSpinnerComponent implements OnInit {
 
+  public loadingPercent = 0;
+
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit():void {
+    this.loadingProgress(500).subscribe(i => this.loadingPercent = i)
   }
 
+
+  // funcao que altera o valor do loadingPercent
+  loadingProgress(speed: number){
+    /**
+     * interval() - uma funcao do rxjs que é tipo o setTimeout.
+     * takeWhile() - para se desinscrever assimq que o i, for menor ou igual a 100.
+     * Quando chegar a 100 deve se desinscrever do observable
+     */
+
+    return interval(speed).pipe(
+      map(i => i * 5),
+      takeWhile(i => i <= 100)
+    )
+  }
 }
