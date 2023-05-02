1560f09ab95e8d8a353e48280f653dedabf3584c
Rita Rizzieri <rita.rizzieri@checklistfacil.com>
2023-05-02 08:35:51 -0300

    solucao no meu desafio

--- src/app/progress-spinner/progress-spinner.component.ts
+++ src/app/progress-spinner/progress-spinner.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ProgressBarMode } from '@angular/material/progress-bar';
-import {concat, interval, map,tap, take, takeWhile} from 'rxjs'
+import {concat, interval, map,tap, take, takeWhile, switchMap, pipe} from 'rxjs'
 
 @Component({
   selector: 'app-progress-spinner',
@@ -12,6 +12,10 @@ export class ProgressSpinnerComponent implements OnInit {
   public loadingPercent = 0;
   public queryMode: ProgressBarMode = 'query';
   public queryValue = 0;
+
+  public bufferValue = 0;
+  public animationEnd: ProgressBarMode = 'buffer'
+  public valueTwo = 0
 
   constructor() { }
 
@@ -26,6 +30,15 @@ export class ProgressSpinnerComponent implements OnInit {
       ),
       this.loadingProgress(500,100)
     ).subscribe(i => this.queryValue = i)
+
+    //playback
+    this.loadingProgress(300, 100).subscribe(i => this.bufferValue = i)
+
+    interval(5000)
+      .pipe(
+        take(1),
+        tap(_ => (this.animationEnd = 'determinate'))
+      ),this.loadingProgress(500,100).subscribe(i => this.valueTwo = i)
   }
 
 
