dca57fa20124cccb188b3d3f22096dc4d538a226
Rita Rizzieri <rita.rizzieri@checklistfacil.com>
2023-06-03 20:54:03 -0300

    criar nova rota do component datatable

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,6 +8,7 @@ import { TabsComponent } from './tabs/tabs.component';
 import { DatepickerComponent } from './datepicker/datepicker.component';
 import { SnackbarComponent } from './snackbar/snackbar.component';
 import { DialogComponent } from './dialog/dialog.component';
+import { DatatableComponent } from './datatable/datatable.component';
 
 const routes: Routes =[
   { path:'buttonIcon', component: ButtonsIconsComponent },
@@ -16,7 +17,8 @@ const routes: Routes =[
   { path: 'tabs', component: TabsComponent },
   { path: 'datepicker', component: DatepickerComponent },
   { path: 'snackbar', component: SnackbarComponent},
-  { path: 'dialog', component: DialogComponent}
+  { path: 'dialog', component: DialogComponent},
+  { path: 'datatable', component: DatatableComponent}
 ]
 
 @NgModule({
