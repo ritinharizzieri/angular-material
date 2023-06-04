fa72f324b1af92084eab3f105e463729c3abcaea
Rita Rizzieri <rita.rizzieri@checklistfacil.com>
2023-06-03 21:35:50 -0300

    adicionando paginação na tabela

--- src/app/datatable/datatable.component.ts
+++ src/app/datatable/datatable.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
 import { DATA_MOVIES } from './model/data-movies';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-datatable',
   templateUrl: './datatable.component.html',
   styleUrls: ['./datatable.component.css']
 })
-export class DatatableComponent {
-  public dataSource = DATA_MOVIES
+export class DatatableComponent implements AfterViewInit {
+  public dataSource = new MatTableDataSource(DATA_MOVIES)
   public displayedNameColumns: string[] = ['name', 'category', 'rating', 'year', 'director']
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+
+  ngAfterViewInit(): void {
+      this.dataSource.paginator = this.paginator
+  }
+
 }
