fbb7e54cc806c84fb43b4c993dd7b2926029219f
Rita Rizzieri <rita.rizzieri@checklistfacil.com>
2023-06-03 21:55:07 -0300

    adiciona filtro na tabela

--- src/app/datatable/datatable.component.ts
+++ src/app/datatable/datatable.component.ts
@@ -21,4 +21,9 @@ export class DatatableComponent implements AfterViewInit {
       this.dataSource.sort = this.sort
   }
 
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
 }
